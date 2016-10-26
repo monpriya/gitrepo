4438f3370639fb20390806acae9616a047fe9dee
monpriya <koiiz.zaa@gmail.com>
2016-10-26 10:01:01 +0700

    from another_pc_gitrepo

--- Login.rb
+++ Login.rb
@@ -32,3 +32,8 @@ Then(/^Click Button Send request for password$/) do
   click_button "Send request for password"
   sleep 2
 end
+
+Then(/^Click Button Send request for password$/) do
+  click_button "Send request for password"
+  sleep 2
+end
