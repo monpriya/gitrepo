208418a16fee6e885603afe6ae4f4e3fdb429ffa
monpriya <koiiz.zaa@gmail.com>
2016-10-26 11:42:34 +0700

    Update Login.rb

--- Login.rb
+++ Login.rb
@@ -1,17 +1,3 @@
-Given(/^user login$/) do
-  visit("https://10.10.5.103/login")
-end
-
-Then(/^user get input Username is "([^"]*)"$/) do |user|
-  expect(page).to have_css('input#username')
-  fill_in('username', :with => 'user1@wavify.org')
-end
-
-Then(/^user get Password is "([^"]*)"$/) do |pass|
-  expect(page).to have_css('input#password.form-control.pull-left')
-  fill_in("Password", :with => "password")
-end
-
 Then(/^Click Button Login$/) do
   click_button "Sign in"
   sleep 2
